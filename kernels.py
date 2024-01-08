82a0ec98870d133fc56fcf10387354861b5ea6f7
LukasBehammer <lukas.behammer@fh-wels.at>
2024-01-08 14:46:39 +0100

    add multidimensional sobel, prewitt and scharr kernels

--- kernels.py
+++ kernels.py
@@ -100,3 +100,78 @@ def gsm_kernel_xy1():
 def gsm_kernel_xy2():
     xy2 = np.rot90(gsm_kernel_yz1(), 1, (0, 2))
     return xy2
+
+
+def sobel_kernel_z():
+    z = np.array([[[0, 0, 0],
+                   [-1, 0, 1],
+                   [0, 0, 0]],
+
+                  [[-1, 0, 1],
+                   [-2, 0, 2],
+                   [-1, 0, 1]],
+
+                  [[0, 0, 0],
+                   [-1, 0, 1],
+                   [0, 0, 0]]])
+    return z
+
+
+def sobel_kernel_y():
+    y = np.rot90(sobel_kernel_z(), 1, (2, 1))
+    return y
+
+
+def sobel_kernel_x():
+    x = np.rot90(sobel_kernel_z(), 1, (2, 0))
+    return x
+
+
+def prewitt_kernel_z():
+    z = np.array([[[0, 0, 0],
+                   [0, 0, 0],
+                   [0, 0, 0]],
+
+                  [[-1, 0, 1],
+                   [-1, 0, 1],
+                   [-1, 0, 1]],
+
+                  [[0, 0, 0],
+                   [0, 0, 0],
+                   [0, 0, 0]]])
+    return z
+
+
+def prewitt_kernel_y():
+    y = np.rot90(prewitt_kernel_z(), 1, (2, 1))
+    return y
+
+
+def prewitt_kernel_x():
+    x = np.rot90(prewitt_kernel_z(), 1, (2, 0))
+    return x
+
+
+def scharr_kernel_z():
+    z = np.array([[[0, 0, 0],
+                   [-47, 0, 47],
+                   [0, 0, 0]],
+
+                  [[-47, 0, 47],
+                   [-162, 0, 162],
+                   [-47, 0, 47]],
+
+                  [[0, 0, 0],
+                   [-47, 0, 47],
+                   [0, 0, 0]]])
+    return z
+
+
+def scharr_kernel_y():
+    y = np.rot90(scharr_kernel_z(), 1, (2, 1))
+    return y
+
+
+def scharr_kernel_x():
+    x = np.rot90(scharr_kernel_z(), 1, (2, 0))
+    return x
