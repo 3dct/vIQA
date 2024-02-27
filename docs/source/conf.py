a5905125778a5c67c15352b52e53949f5ce901c8
LukasBehammer <lukas.behammer@fh-wels.at>
2024-02-27 18:23:14 +0100

    Update version to 0.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'vIQA'
 copyright = '2024, Lukas Behammer'
 author = 'Lukas Behammer'
-release = '0.3.0'
+release = '0.4.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
