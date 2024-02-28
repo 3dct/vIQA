8a0d6d2dff40d1c2052e5ac4ce23cb6d0f5767dc
LukasBehammer <lukas.behammer@fh-wels.at>
2024-02-28 17:34:29 +0100

    Delete _extract_blocks from __inity.py__

--- src/viqa/__init__.py
+++ src/viqa/__init__.py
@@ -8,4 +8,4 @@ __author__ = "Lukas Behammer"
 
 from .metrics import *
 from .kernels import *
-from .utils import load_data, normalize_data, extract_blocks
+from .utils import load_data, normalize_data
