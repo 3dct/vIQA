127252f79ed1077ca920eebb971f3b2d828b077e
LukasBehammer <lukas.behammer@fh-wels.at>
2024-03-05 13:22:03 +0100

    Add no reference metrics

--- src/viqa/__init__.py
+++ src/viqa/__init__.py
@@ -1,11 +1,11 @@
 """
-vIQA (voulmetric Image Quality Assessment) is a Python package for the assessment of volumetric image quality.
+vIQA (volumetric Image Quality Assessment) is a Python package for the assessment of volumetric image quality.
 It provides a set of metrics to assess the quality of volumetric images. The package can be used for 2D and 3D images.
 """
 
 __version__ = "0.6.0"
 __author__ = "Lukas Behammer"
 
-from .metrics import *
+from .fr_metrics import *
 from ._kernels import *
 from .utils import load_data, normalize_data
--- src/viqa/_metrics.py
+++ src/viqa/_metrics.py
@@ -42,3 +42,46 @@ class FullReferenceMetricsInterface(ABC):
 
     def __repr__(self):
         return f"{self.__class__.__name__}(score_val={self.score_val})"
+
+
+class NoReferenceMetricsInterface(ABC):
+    def __init__(self, data_range, normalize, batch, **kwargs):
+        self._parameters = {
+            "data_range": data_range,
+            "normalize": normalize,
+            "batch": batch,
+            "chromatic": False,
+            **kwargs,
+        }
+        self.score_val = None
+        if self._parameters["normalize"] and not self._parameters["data_range"]:
+            raise ValueError("If normalize is True, data_range must be specified")
+
+    @abstractmethod
+    def score(self, img):
+        pass
+
+    @abstractmethod
+    def print_score(self):
+        pass
+
+    def __eq__(self, other):
+        return self.score_val == other.score_val
+
+    def __lt__(self, other):
+        return self.score_val < other.score_val
+
+    def __gt__(self, other):
+        return self.score_val > other.score_val
+
+    def __le__(self, other):
+        return self.score_val <= other.score_val
+
+    def __ge__(self, other):
+        return self.score_val >= other.score_val
+
+    def __ne__(self, other):
+        return self.score_val != other.score_val
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(score_val={self.score_val})"
