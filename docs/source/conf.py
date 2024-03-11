9008a40c640a735246a2d601a988cd7a8aa1bf30
LukasBehammer <lukas.behammer@fh-wels.at>
2024-03-11 17:49:56 +0100

    Update doc building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
     'sphinx_copybutton',
     'pytest_doctestplus.sphinx.doctestplus',
 ]
