f8125b7a72fcfc63c8072c1dc92499bf972f0573
Lukas Behammer <131662795+lukasbehammer@users.noreply.github.com>
2024-03-12 13:28:17 +0100

    Update version

--- src/viqa/__init__.py
+++ src/viqa/__init__.py
@@ -3,7 +3,7 @@ vIQA (volumetric Image Quality Assessment) is a Python package for the assessmen
 It provides a set of metrics to assess the quality of volumetric images. The package can be used for 2D and 3D images.
 """
 
-__version__ = "0.8.1"
+__version__ = "0.8.2dev0"
 __author__ = "Lukas Behammer"
 
 from .fr_metrics import *
