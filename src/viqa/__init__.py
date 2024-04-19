177b183f451bd8c34f4d9011541947f52da361da
Lukas Behammer <131662795+lukasbehammer@users.noreply.github.com>
2024-04-19 15:22:58 +0200

    Exclude rule D301 in ruff linter

--- src/viqa/__init__.py
+++ src/viqa/__init__.py
@@ -6,7 +6,7 @@ The package provides a set of metrics to assess the quality of volumetric images
 It can be used for 2D and 3D images.
 """
 
-__version__ = "0.13.4"
+__version__ = "0.13.5"
 __author__ = "Lukas Behammer"
 __all__ = ["FSIM", "GSM", "MAD", "MSSSIM", "PSNR", "PSNR", "RMSE", "SSIM", "VIFp",
            "VSI", "CNR", "SNR", "load_data", "normalize_data", "export_csv",
