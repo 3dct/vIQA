5e8fbe3b7fba9c57321e6fbf071d9fbee3fb8285
Lukas Behammer <131662795+lukasbehammer@users.noreply.github.com>
2024-04-19 15:30:34 +0200

    Add deprecation docstrings

--- src/viqa/deprecation.py
+++ src/viqa/deprecation.py
@@ -2,10 +2,14 @@
 
 
 class RemovedInViqa10Warning(DeprecationWarning):
+    """Warn about features that will be removed in ViQa 1.0."""
+
     pass
 
 
 class RemovedInViqa20Warning(PendingDeprecationWarning):
+    """Warn about features that will be removed in ViQa 2.0."""
+
     pass
 
 
