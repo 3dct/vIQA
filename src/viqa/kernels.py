e5ac9a65ccd81f5dc34d6f761a59e4d9e12501cc
Lukas Behammer <131662795+lukasbehammer@users.noreply.github.com>
2024-04-19 15:25:55 +0200

    Add copyright and module docstring to kernels.py

--- src/viqa/kernels.py
+++ src/viqa/kernels.py
@@ -1,3 +1,20 @@
+"""Module to provide kernels for image processing."""
+
+# Authors
+# -------
+# Author: Lukas Behammer
+# Research Center Wels
+# University of Applied Sciences Upper Austria, 2023
+# CT Research Group
+#
+# Modifications
+# -------------
+# Original code, 2024, Lukas Behammer
+#
+# License
+# -------
+# BSD-3-Clause License
+
 import numpy as np
 
 
