3422cb430e6566278460d7346725ab12afca186d
LukasBehammer <lukas.behammer@fh-wels.at>
2024-05-06 14:39:00 +0200

    Fix QMeasure

--- src/viqa/fr_metrics/qmeasure_calc/qmeasurecalcmodule.cpp
+++ src/viqa/fr_metrics/qmeasure_calc/qmeasurecalcmodule.cpp
@@ -12,7 +12,8 @@
  */
 
 #define PY_SSIZE_T_CLEAN
-#include <Python.h>
+//#include <Python.h>
+#include "C:\Users\p42938\.conda\envs\IQA_Library\include\Python.h"
 #include <numpy/arrayobject.h>
 #include "qmeasurecalcmodule.h"
 #include "iAQMeasureCalculation.h"
@@ -20,7 +21,7 @@
 // Method Table
 static PyMethodDef qmeasureMethods[] = {
         {"qmeasure", qmeasure, METH_VARARGS,
-                "qmeasure(image: np.ndarray, data_range: int, hist_bins: int, num_peaks: int, analyze_peak: bool) -> float\n\n"
+                "qmeasure(image: np.ndarray, min: int, max: int, hist_bins: int, num_peaks: int) -> float\n\n"
                 "Calculate the Q Measure."},
         {NULL, NULL, 0, NULL}
 };
@@ -45,33 +46,37 @@ static PyObject
 {
     // Declare variables
     PyArrayObject *matin;
-    float *data;
     double qvalue;
-    int idatarange, ihistbins, inumpeaks;
+    int imin, imax, ihistbins, inumpeaks;
     bool banalyzepeak;
-    npy_intp dims[3];
+    int dims[3];
 
     // Parse input arguments
-    if (!PyArg_ParseTuple(args, "O!iiib", &PyArray_Type, &matin, &idatarange, &ihistbins, &inumpeaks, &banalyzepeak)) {
+    if (!PyArg_ParseTuple(args, "O!iiii", &PyArray_Type, &matin, &imin, &imax, &ihistbins, &inumpeaks)) {
         return NULL;
     }
     if (NULL == matin) return NULL;
-    // Get dimensions of input array
-    dims[0] = matin->dimensions[0];
-    dims[1] = matin->dimensions[1];
-    dims[2] = matin->dimensions[2];
 
+    // Get dimensions of input array and cast to int
+    dims[0] = static_cast<int>(matin->dimensions[0]);
+    dims[1] = static_cast<int>(matin->dimensions[1]);
+    dims[2] = static_cast<int>(matin->dimensions[2]);
+
+    // Get data range
     double data_range[2];
-    data_range[0] = 0;
-    data_range[1] = idatarange;
+    data_range[0] = imin;
+    data_range[1] = imax;
 
-    data = (float *) matin->data;
+    // Get data from input array
+    auto data = (float *) PyArray_DATA(matin);
 
     // Call the C++ function
     iAQMeasureCalculation iAQMeasureCalculation;
-    auto result = iAQMeasureCalculation.computeOrigQ(data, reinterpret_cast<const int *>(dims), data_range, ihistbins, inumpeaks, banalyzepeak);
+    auto result = iAQMeasureCalculation.computeOrigQ(data, dims, data_range, ihistbins, inumpeaks, false);
 
+    // Get Q value from map
     qvalue = result["Q (orig, equ 1)"];
+
     // Return output array
     return PyFloat_FromDouble(qvalue);
 }
