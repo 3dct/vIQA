73062fcf28bbbefd5b11f5c4be1e1cc3027e9c65
p42938 <lukas.behammer@fh-wels.at>
2024-06-11 17:26:41 +0200

    Bugfix in snr.py:signal_to_noise_ratio to set snr to 0 if stdev = 0

--- src/viqa/nr_metrics/snr.py
+++ src/viqa/nr_metrics/snr.py
@@ -321,7 +321,7 @@ def signal_to_noise_ratio(img, signal_center, radius, yuv=True):
         raise ValueError("Image has to be either 2D or 3D.")
 
     # Calculate SNR
-    if np.mean(signal) == 0:
+    if np.std(signal) == 0:
         snr_val = 0
     else:
         snr_val = np.mean(signal) / np.std(signal)
