f447976d940f3bda632b87e41ffbd65f59496fe5
lukasbehammer <lukas.behammer@fh-wels.at>
2024-07-03 14:55:33 +0200

    Update get_numpy_path.py

--- src/viqa/_build_utils/get_numpy_path.py
+++ src/viqa/_build_utils/get_numpy_path.py
@@ -8,6 +8,12 @@
 # os.chdir("..")
 # print(os.path.relpath(numpy.get_include().replace("\\", "/")))
 
+import os
+from pathlib import Path
+
+home_path = Path.home()
+
+incdir_numpy = os.path.join(home_path, 'AppData/Local/anaconda3/Lib/site-packages/numpy/core/include')
 # incdir_numpy = 'C:/ProgramData/anaconda3/Lib/site-packages/numpy/core/include'
-incdir_numpy = '/opt/conda/lib/python3.11/site-packages/numpy/core/include'
+# incdir_numpy = '/opt/conda/lib/python3.11/site-packages/numpy/core/include'
 print(incdir_numpy)
