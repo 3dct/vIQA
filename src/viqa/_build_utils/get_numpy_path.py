23bba7c4516d9e4d5c10eb47ebd26ab45e2adbf8
lukasbehammer <lukas.behammer@fh-wels.at>
2024-07-08 14:42:25 +0200

    Update build process

--- src/viqa/_build_utils/get_numpy_path.py
+++ src/viqa/_build_utils/get_numpy_path.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
 """Get numpy include path."""
 
-# import os
-#
-# import numpy
-#
-# os.chdir("..")
-# print(os.path.relpath(numpy.get_include().replace("\\", "/")))
-
 import os
-from pathlib import Path
 
-home_path = Path.home()
+try:
+    import numpy
+except ImportError:
+    print("numpy is not installed")
+    exit(20)
 
-incdir_numpy = os.path.join(home_path, 'AppData/Local/anaconda3/Lib/site-packages/numpy/core/include')
-# incdir_numpy = 'C:/ProgramData/anaconda3/Lib/site-packages/numpy/core/include'
-# incdir_numpy = '/opt/conda/lib/python3.11/site-packages/numpy/core/include'
-print(incdir_numpy)
+os.chdir("..")
+print(numpy.get_include().replace("\\", "/"))
