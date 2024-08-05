12e20acfd494ea47d75cff1249b37b95e01245e6
lukasbehammer <lukas.behammer@fh-wels.at>
2024-08-05 16:19:36 +0200

    feat: add function to get installed version

--- src/viqa/__init__.py
+++ src/viqa/__init__.py
@@ -6,7 +6,7 @@ The package provides a set of metrics to assess the quality of volumetric images
 It can be used for 2D and 3D images.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = "Lukas Behammer"
 __all__ = ["FSIM", "GSM", "MAD", "UQI", "MSSSIM", "PSNR", "PSNR", "RMSE", "SSIM",
            "VIFp", "VSI", "CNR", "SNR", "QMeasure", "load_data", "normalize_data",
@@ -19,3 +19,15 @@ from .fusion import fuse_metrics_linear_combination
 from .load_utils import ImageArray, load_data, normalize_data
 from .nr_metrics import *
 from .utils import export_csv
+
+
+def get_version():
+    """
+    Return the version of the package.
+
+    Returns
+    -------
+    str
+        The version of the package
+    """
+    return __version__
