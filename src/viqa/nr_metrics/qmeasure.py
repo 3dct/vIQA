5f589c1b13c8584e1f1ee618b5b9ee46533206dc
lukasbehammer <lukas.behammer@fh-wels.at>
2024-10-29 12:48:22 +0100

    fix(qmeasure): fix image loading in score method

--- src/viqa/nr_metrics/qmeasure.py
+++ src/viqa/nr_metrics/qmeasure.py
@@ -128,7 +128,7 @@ class QMeasure(NoReferenceMetricsInterface):
         score_val : float
             Q-Measure value.
         """
-        img = super().score(img)
+        img = self.load_images(img)
 
         if img.ndim < 3:
             raise ValueError("Q-Measure is only defined for 3D CT volumes.")
